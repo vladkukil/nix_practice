2cfcd93dedb9651840aec3ce37aec355bab1a623
vladkukil <kukilvladislav7@gmail.com>
2021-03-05 14:57:02 +0200

    Add php file for lesson 3

---
+++ html/index.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NIX Education</title>
+
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+<body>
+    <header class='header'>
+        
+    </header>
+
+    <div class='navbar'>
+        <div class="logo"><a href="#">NIX Education</a></div>
+        <div class="nav">
+            <ul>
+                <li><a href='../html/multiplication.php'>Урок 1</a></li>
+                <li><a href='../html/multiplication-color.php'>Урок 2</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class='main'>
+        
+    </div>
+
+    <footer class='footer'>
+
+    </footer>
+</body>
+</html>
--- html/multiplication-color.php
+++ html/multiplication-color.php
@@ -17,43 +17,37 @@
         <div class="logo"><a href="../html/index.php">NIX Education</a></div>
         <div class="nav">
             <ul>
-                <li><a href='#'>Урок 1</a></li>
-                <li><a href='../html/multiplication-color.php'>Урок 2</a></li>
+                <li><a href='../html/multiplication.php'>Урок 1</a></li>
+                <li><a href='#'>Урок 2</a></li>
             </ul>
         </div>
     </div>
 
     <div class='main'>
         <?php 
-            $rows = 2;
-            $cols = 5;
-            $mult = 10;
-            $first_numb = 1;
-            $second_number = 1;
-            $result;
+        function color_table($rows, $cols, $first_number, $second_number, $result) {
             echo '<table border="1">';
             for($tr = 1; $tr <= $rows; $tr++){
                 echo '<tr>';
-                for($td=1; $td<=$cols; $td++){
+                for($td = 1; $td <= $cols; $td++){
                     echo '<td>';
-                    for($a = 1; $a<=$mult; $a++){
+                    for($a = 1; $a <= 10; $a++){
                         if($second_number == 11){
-                            $first_numb++;
+                            $first_number++;
                             $second_number = 1;
                         }
-                            $result = $first_numb * $second_number;
-                            echo "$first_numb x $second_number = $result";
-                            echo "<br>";
-                            $second_number++;
-                        }
+                        $result = $first_number * $second_number;    
+                        echo "$first_number x $second_number = $result <br>";
+                        $second_number++;
                     }
-                    echo '</td>';
                 }
-                echo '</tr>';
-                
-                
-            
+                    echo '</td>';
+            }
+            echo '</tr>'; 
             echo '</table>';
+        }
+            echo color_table(2, 5, 1, 1, 0);
+        
         ?>
     </div>
 
